8a012320479dff82d2282bee5aa7f70b7bbde6cf
ezikechris <christophermbe1009@gmail.com>
2020-12-15 11:15:41 +0100

    intial commit

---
+++ Random.py
@@ -0,0 +1,14 @@
+# RANDOMIZATION IS A PROCESS WITH WHICH ONE USES TO CREATE RANDOM NUMBER IN PYTHON #
+
+import random
+
+c = random.randint(2, 10)
+print(c) 
+
+# Random module was created by the python team to make it easier to generate random number 
+# without the need for all of the complexity needed to generate sudo randmom numbers#
+
+
+
+
+
