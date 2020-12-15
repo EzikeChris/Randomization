f8182ed025a4485282d3f9bceed928857886dc3c
ezikechris <christophermbe1009@gmail.com>
2020-12-15 13:24:17 +0100

    intial commit

---
+++ NamingVariables.py
@@ -0,0 +1,19 @@
+# THEY ARE MULTIPLE WAYS OF NAMING VARIABLES IN PYTHON #
+
+# EXAMPLES BELOW #
+
+# CAMEL CASES #
+
+secondNumber = 1
+SecondNumber = 1
+second_number = 1 # the best and requrired for readable name #
+
+# NOTE VARIABLE NAMES CANT START WITH A NUMBER #
+
+# ALWAYS START WITH A LETTER #
+
+
+# YOU CAN CHECK THE VARIABLE TYPE USING THE THE TYPE FUNCTION #
+number = 1
+type(number)
+print(number)
